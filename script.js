8b6c7633e78f8a294c7aaf52eec44c9e72a0e00e
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-08 19:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+let c=0;
+let ch='';
+for(int i=0;i<str.length()-1;i++){
+	let ct=0;
+	for(int j=i+1;j<str.length();j++){
+		if(str.charAt(i)==str.charAt(j)){
+			ct++;
+			continue;
+		}
+		if(ct==0){
+			c++;
+			ch=str.charAt(i);
+		}
+		i=j;
+		
+	}
+}
+if(c==1){
+	console.log(ch);
+}
+else{
+	console.log("null");
+}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
